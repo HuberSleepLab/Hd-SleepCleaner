cac52c6994a4eccf38201c3e1e4d74d84e154642
Sven Leach <info@svenno.de>
2022-02-02 11:40:39 +0100

    [MOD] Takes previous arntdxn into account

--- functions/deviationEEG.m
+++ functions/deviationEEG.m
@@ -1,4 +1,4 @@
-function [devEEG] = deviationEEG(EEG, srate, winlen)
+function [devEEG] = deviationEEG(EEG, srate, winlen, artndxn)
 
 nPnts     = size(EEG,2);                            % sample points
 nEpo20    = floor(nPnts/srate/winlen);              % number of 20s epochs
@@ -12,10 +12,12 @@ if ~isempty(infchans)
     EEG(infchans, :) = nan;
 end
 
-
 for epo = 1:floor(nPnts/srate/winlen)
     XT              = epo * winlen * srate + 1 - winlen*srate : epo * winlen * srate;         
     devEEG(:, epo)  = max( abs( EEG(:, XT) - mean(EEG(:, XT), 'omitnan') ).^2, [], 2);   
 end
 
+if ~isempty(artndxn)
+    SWA(~artndxn) = nan;
+end
 end
