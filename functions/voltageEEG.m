c79bf1e44148b1b0e493552bdd09f2e19c5f750f
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-06-13 13:34:56 +0200

    create cleaning criteria based on mean voltage

---
+++ functions/voltageEEG.m
@@ -0,0 +1,28 @@
+function [voltEEG] = voltageEEG(EEG, srate, winlen, artndxn, chans_excl)
+
+    [nCh, nPnts] = size(EEG);                           % sample points
+    nEpochs      = floor(nPnts/srate/winlen);
+    voltEEG       = zeros(nCh, nEpochs);
+    
+    % Identify infinity values and replace values
+    % Otherwise pwelch won't work
+    infchans = find(any(isinf(EEG), 2));
+    if ~isempty(infchans)
+        fprintf('Channels %g have a 0 line', infchans)
+        EEG(infchans, :) = nan;
+    end
+    
+    for epo = 1:nEpochs
+        XT              = epo * winlen * srate + 1 - winlen*srate : epo * winlen * srate;     
+        voltEEG(:, epo)  = mean(abs(EEG(:, XT)), 2, 'omitnan');
+    end
+    
+    if ~isempty(artndxn)
+        voltEEG(~artndxn) = nan;
+    end
+
+
+    % set to nan all channels to exclude
+    voltEEG(chans_excl, :) = nan;
+
+end
